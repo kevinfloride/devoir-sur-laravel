0700f3258a752a2b6f1b5ebd2607305f2e9564b6
kevinfloride <82867963+kevinfloride@users.noreply.github.com>
2022-04-23 12:36:40 +0100

    Add files via upload

---
+++ master.blade.php
@@ -0,0 +1,17 @@
+html>
+    <head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Tutoriel Laravel 9 gratuit">
+    <meta name="keywords" content="HTML, CSS, JavaScript">
+    <meta name="author" content="John Doe">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+
+        @section('sidebar')
+
+        <div class="container">
+        <p>Corps du contenu.</p>
+        </div>
+    </body>
+</html>
